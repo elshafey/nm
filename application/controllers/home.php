fe4504ae6fa0ccec3ddfccc807c8cb6013a6a0cd
Mohamed Elshafey <m.elshafey@basharsoft.com>
2013-10-02 19:27:22 +0200

    solving comments

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -317,7 +317,7 @@ class Home extends My_Controller {
                 '<meta property="og:url" content="' . base_url() . 'home/preview_book/' . $id . '"/>' .
                 '<meta property="og:image" content="' . base_url() . $this->data['book']['img'] . '" />' .
                 '<meta property="og:site_name" content="Nahdet Misr"/>' .
-                '<meta property="og:description" content="' . strip_tags(htmlentities($this->data['book']['brief_description'][get_locale()])) . '"/>';
+                '<meta property="og:description" content="' . html_entity_decode(strip_tags($this->data['book']['brief_description'][get_locale()])) . '"/>';
 
         $this->data['page_title'] = $this->data['book']['title'][get_locale()];
         $this->data['navigator'][] = '<span class="sub-item"> &gt; ' . $this->data['page_title'] . '</span>';
