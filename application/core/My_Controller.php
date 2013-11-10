05ece9434f8fc2c962628c148775ffd3c47605fb
Mohamed Elshafey <m.elshafey@basharsoft.com>
2013-11-10 16:53:16 +0200

    solving comments

--- application/core/My_Controller.php
+++ application/core/My_Controller.php
@@ -1,64 +1,75 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 /*
-| -------------------------------------------------------------------
-| My Controller
-| -------------------------------------------------------------------
-| This file represent parent class for all system controllers.
-|
-| The file contains global shared functions among all controllers and extend 
-| Codeigniter Controller. Fucntions such as reading configuration, settings
-| localizations and layout, reading global url request should goes here.
+  | -------------------------------------------------------------------
+  | My Controller
+  | -------------------------------------------------------------------
+  | This file represent parent class for all system controllers.
+  |
+  | The file contains global shared functions among all controllers and extend
+  | Codeigniter Controller. Fucntions such as reading configuration, settings
+  | localizations and layout, reading global url request should goes here.
  */
- /**
-  * @property Doctrine $doctrine 
-  * @property CI_Session $session 
-  * @property CI_Template $template
-  * @property My_Form_validation $form_validation 
-  * @property CI_URI $uri
-  * @property CI_Lang $lang
-  * @property CI_Loader $load
-  * @property CKEditor $ckeditor
-  * @property CI_Email $email
-  * 
-  */
+
+/**
+ * @property Doctrine $doctrine 
+ * @property CI_Session $session 
+ * @property CI_Template $template
+ * @property My_Form_validation $form_validation 
+ * @property CI_URI $uri
+ * @property CI_Lang $lang
+ * @property CI_Loader $load
+ * @property CKEditor $ckeditor
+ * @property CI_Email $email
+ * @property CI_Router $router
+ * 
+ */
 class My_Controller extends CI_Controller {
-    
+
     var $data;
 
-    public function __construct()
-    {
+    public function __construct() {
         //Extending parent construction
         parent::__construct();
-        
+
         //Init class memebers
         $this->data = array();
-        
+
         //Init local configurations
         if (is_readable(FCPATH . 'application/config/config.local.php')) {
             $this->config->load('config.local');
         }
-        
+
+        if ($this->router->method == 'preview_book') {
+            $exp = explode('-', $this->uri->segment(3));
+            if (count($exp) > 1) {
+                $id = array_shift($exp);
+                $lang = implode('-', $exp);
+                $this->session->set_userdata('user_locale', $lang);
+            }
+        }
+
         set_locale();
         //Local settings override permanent settings always if found.
 
         flash_message();
     }
-    
-    
+
     public function set_var($name, $value) {
-        $this->data[$name]=$value;
+        $this->data[$name] = $value;
     }
-    
-    public function get_var($name)
-    {
-        if(isset ($this->data[$name]))
+
+    public function get_var($name) {
+        if (isset($this->data[$name]))
             return $this->data[$name];
-        
+
         return "";
     }
+
 }
- 
+
 /* End of file: My_Controller */
 /* Location: ./application/core/My_Controller.php */
-
 ?>
